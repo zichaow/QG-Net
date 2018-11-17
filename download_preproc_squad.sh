1c9c7c1484d5c0927dc47a5d535d6a8eb5627c79
moonlightlane <wangzichao6@gmail.com>
2018-11-17 20:56:55 +0000

    updated preprocessed squad data downloading

--- download_preproc_squad.sh
+++ download_preproc_squad.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # Configure download location
-
-# first download the QG-Net model
-DOWNLOAD_PATH="test/models.for.test/"
+DOWNLOAD_PATH="data"
 mkdir -p $DOWNLOAD_PATH
+cd $DOWNLOAD_PATH
 
 # Download the train and dev squad data
 # test split already downloaded with the download_QG-Net.sh script
-cd data/
 
-wget -v -O train.tar.gz -L https://rice.box.com/shared/static/qhyc8ytc8ikfeuv16s5vcaismum48zle.gz
-tar -xvf train.tar.gz
+wget -v -O opennmt-input-data.tar.gz -L https://rice.box.com/shared/static/6haddoiep15fmdqmtdp3ccf44o1m6e4z.gz 
+tar -xvf opennmt-input-data.tar.gz
 
-wget -v -O dev.tar.gz -L https://rice.box.com/shared/static/blnqyfrfdw0tubaguslzutaa4fk1wdcn.gz
+#wget -v -O train.tar.gz -L https://rice.box.com/shared/static/qhyc8ytc8ikfeuv16s5vcaismum48zle.gz
+#tar -xvf train.tar.gz
+
+#wget -v -O dev.tar.gz -L https://rice.box.com/shared/static/blnqyfrfdw0tubaguslzutaa4fk1wdcn.gz
+#tar -xvf dev.tar.gz
 
 cd ../
 
-
-echo "download completed."
-
+echo "preprocessed data download completed."
 
